9de7333fefe569734d04faf4d21ad290eda1a1eb
Azi Chen <azi.chen@abagile.com>
2022-09-23 00:38:48 +0800

    simplify code round2

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,5 +1,4 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
@@ -7,23 +6,10 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       case item.name
-      when "Aged Brie"
-        item.sell_in -= 1
-        item.quality += 1 if item.quality < 50
-        item.quality += 1 if item.sell_in < 0 && item.quality < 50
-      when "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality < 50
-          item.quality += 1
-          item.quality += 1 if item.sell_in < 11 && item.quality < 50
-          item.quality += 1 if item.sell_in < 6 && item.quality < 50
-        end
-        item.sell_in -= 1
-        item.quality = 0 if item.sell_in < 0
+      when "Aged Brie"                                 then calc_brie(item)
+      when "Backstage passes to a TAFKAL80ETC concert" then calc_concer(item)
       when "Sulfuras, Hand of Ragnaros"
-      else
-        item.sell_in -= 1
-        item.quality -= 1 if item.quality > 0
-        item.quality -= 1 if item.sell_in < 0 && item.quality > 0
+      else                                                  calc_default(item)
       end
     end
   end
@@ -41,4 +27,26 @@ class Item
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
   end
+end
+
+def calc_brie(item)
+  item.sell_in -= 1
+  item.quality += 1 if item.quality < 50
+  item.quality += 1 if item.sell_in < 0 && item.quality < 50
+end
+
+def calc_concer(item)
+  if item.quality < 50
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 11 && item.quality < 50
+    item.quality += 1 if item.sell_in < 6 && item.quality < 50
+  end
+  item.sell_in -= 1
+  item.quality = 0 if item.sell_in < 0
+end
+
+def calc_default(item)
+  item.sell_in -= 1
+  item.quality -= 1 if item.quality > 0
+  item.quality -= 1 if item.sell_in < 0 && item.quality > 0
 end
