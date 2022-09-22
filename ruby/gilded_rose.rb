0df4134924285ce324adc361565304718c1b3b15
Azi Chen <azi.chen@abagile.com>
2022-09-23 00:20:11 +0800

    simplify code

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,11 +6,10 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-
       case item.name
       when "Aged Brie"
+        item.sell_in -= 1
         item.quality += 1 if item.quality < 50
-        item.sell_in = item.sell_in - 1
         item.quality += 1 if item.sell_in < 0 && item.quality < 50
       when "Backstage passes to a TAFKAL80ETC concert"
         if item.quality < 50
@@ -18,17 +17,13 @@ class GildedRose
           item.quality += 1 if item.sell_in < 11 && item.quality < 50
           item.quality += 1 if item.sell_in < 6 && item.quality < 50
         end
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0
-          item.quality = 0
-        end
+        item.sell_in -= 1
+        item.quality = 0 if item.sell_in < 0
       when "Sulfuras, Hand of Ragnaros"
       else
+        item.sell_in -= 1
         item.quality -= 1 if item.quality > 0
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0 && item.quality > 0
-          item.quality -= 1
-        end
+        item.quality -= 1 if item.sell_in < 0 && item.quality > 0
       end
     end
   end
