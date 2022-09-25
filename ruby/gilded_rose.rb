7580fb92fa0d86e9812098eaa0fa1bac22f156d1
Azi Chen <azi.chen@abagile.com>
2022-09-25 23:30:06 +0800

    Simplify code round3

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -31,22 +31,25 @@ end
 
 def calc_brie(item)
   item.sell_in -= 1
-  item.quality += 1 if item.quality < 50
-  item.quality += 1 if item.sell_in < 0 && item.quality < 50
+  offset       = (item.sell_in < 0) ? 2 : 1
+  item.quality += offset if item.quality < 50
 end
 
 def calc_concer(item)
-  if item.quality < 50
-    item.quality += 1
-    item.quality += 1 if item.sell_in < 11 && item.quality < 50
-    item.quality += 1 if item.sell_in < 6 && item.quality < 50
-  end
+  offset = case
+           when item.sell_in < 11 && item.sell_in >= 6 then 2
+           when item.sell_in < 6  && item.sell_in > 0  then 3
+           when item.sell_in == 0                      then -1 * item.quality
+           else 1
+           end
+
+  item.quality += offset if item.quality < 50
+  item.quality = 50      if item.quality >= 50
   item.sell_in -= 1
-  item.quality = 0 if item.sell_in < 0
 end
 
 def calc_default(item)
   item.sell_in -= 1
-  item.quality -= 1 if item.quality > 0
-  item.quality -= 1 if item.sell_in < 0 && item.quality > 0
+  offset       = (item.sell_in < 0 ) ? 2 : 1
+  item.quality -= offset if item.quality > 0
 end
