13f85baf02ec225cb1323fec2a85a72728e37d1d
Azi Chen <azi.chen@abagile.com>
2022-09-27 00:56:34 +0800

    refactor use extend module

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,3 +1,40 @@
+require "active_support/core_ext/string"
+
+module Handler
+  def update_quality
+    @sell_in -= 1
+    offset   = (@sell_in < 0 ) ? 2 : 1
+    @quality -= offset if @quality > 0
+  end
+
+  module Brie
+    def update_quality
+      @sell_in -= 1
+      offset  = (@sell_in < 0) ? 2 : 1
+      @quality += offset if @quality < 50
+    end
+  end
+
+  module Concert
+    def update_quality
+      @sell_in -= 1
+      offset = case
+               when @sell_in >= 10 then 1
+               when @sell_in >=  5 then 2
+               when @sell_in >=  0 then 3
+               when @sell_in <   0 then -1 * @quality
+               end
+
+      @quality += offset if @quality < 50
+      @quality = 50      if @quality >= 50
+    end
+  end
+
+  module Ragnaros
+    def update_quality ;end
+  end
+end
+
 class GildedRose
   def initialize(items)
     @items = items
@@ -5,51 +42,24 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      case item.name
-      when "Aged Brie"                                 then calc_brie(item)
-      when "Backstage passes to a TAFKAL80ETC concert" then calc_concer(item)
-      when "Sulfuras, Hand of Ragnaros"
-      else                                                  calc_default(item)
-      end
+      item.update_quality
     end
   end
 end
 
 class Item
+  include Handler
   attr_accessor :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
-    @name = name
+    @name    = name
     @sell_in = sell_in
     @quality = quality
+    handler  = "Handler::#{@name.split(' ').last.camelize}".safe_constantize
+    extend handler if handler
   end
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
   end
-end
-
-def calc_brie(item)
-  item.sell_in -= 1
-  offset       = (item.sell_in < 0) ? 2 : 1
-  item.quality += offset if item.quality < 50
-end
-
-def calc_concer(item)
-  item.sell_in -= 1
-  offset = case
-           when item.sell_in >= 10 then 1
-           when item.sell_in >=  5 then 2
-           when item.sell_in >=  0 then 3
-           when item.sell_in <   0 then -1 * item.quality
-           end
-
-  item.quality += offset if item.quality < 50
-  item.quality = 50      if item.quality >= 50
-end
-
-def calc_default(item)
-  item.sell_in -= 1
-  offset       = (item.sell_in < 0 ) ? 2 : 1
-  item.quality -= offset if item.quality > 0
 end
